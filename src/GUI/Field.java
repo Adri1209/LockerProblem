af699f40a2f31abb75345481b3f1cf201ba94fd5
Adrian Haase <adrian.hasse1@web.de>
2018-03-04 12:23:15 +0100

    added dropdown List with listener. By changing value, lockers will change immediately

--- src/GUI/Field.java
+++ src/GUI/Field.java
@@ -6,11 +6,23 @@ import javafx.scene.shape.Rectangle;
 
 public class Field extends StackPane {
 
+    private Rectangle rectangle;
+
     public Field()  {
-        Rectangle rectangle = new Rectangle(5, 5);
+        rectangle = new Rectangle(5, 5);
         rectangle.setFill(Color.BLACK);
         rectangle.setStroke(Color.WHITE);
 
         getChildren().add(rectangle);
+    }
+
+    public void setStroke (Color color){
+
+        rectangle.setStroke(color);
+    }
+
+    public void setFill (Color color){
+
+        rectangle.setFill(color);
     }
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -3,7 +3,9 @@ package GUI;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -21,6 +23,8 @@ public class Main extends Application {
         root.setPrefSize(600,600);
 
 
+
+
         List<Field> fields = new ArrayList<>();
         for (int i = 0; i < 10000; i++) {
             fields.add(new Field());
@@ -29,9 +33,21 @@ public class Main extends Application {
         for (int i = 0; i < fields.size(); i++) {
             Field field = fields.get(i);
             field.setTranslateX((5 * (i % 100)) + 50);
-            field.setTranslateY((5 * (i / 100)) + 50);
+            field.setTranslateY((5 * (i / 100)) + 70);
             root.getChildren().add(field);
         }
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.setTranslateX(50);
+        choiceBox.setTranslateY(25);
+        choiceBox.getItems().add("square frees");
+        choiceBox.getItems().add("cube time square frees");
+        choiceBox.setValue("square frees");
+
+        root.getChildren().add(choiceBox);
+
+        changeFieldsDependingOnSetting(choiceBox.getValue(),fields);
+        choiceBox.getSelectionModel().selectedItemProperty().addListener((v, oldvalue, newValue) -> changeFieldsDependingOnSetting(newValue,fields));
 
         return root;
     }
@@ -45,6 +61,23 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    private void changeFieldsDependingOnSetting(String value, List<Field> fields){
+
+        if (value.equals("square frees")){
+
+            for (Field field: fields){
+                field.setFill(Color.BLACK);
+                field.setStroke(Color.WHITE);
+            }
+        }
+        if (value.equals("cube time square frees")){
+            for (Field field: fields){
+                field.setFill(Color.WHITE);
+                field.setStroke(Color.BLACK);
+            }
+        }
     }
 }
 
