6b1156533fa5e20a1452436d4e91c2eceb631921
Annika <annika.schatz21@web.de>
2018-03-19 11:28:51 +0100

    containsExactly2 gives right values back

--- src/Logic/Evaluate.java
+++ src/Logic/Evaluate.java
@@ -4,28 +4,26 @@ import java.util.*;
 
 public class Evaluate {
 
-    public boolean containsMore(ArrayList<Long> factors){
+    public boolean containsMore(ArrayList<Long> factors) {
         Set<Long> set = new HashSet<Long>(factors);
-        if(set.size()< factors.size() && !factors.isEmpty()){
+        if (set.size() < factors.size() && !factors.isEmpty()) {
             return true;
         }
         return false;
 
     }
 
-    public boolean conatinsExactly2(ArrayList<Long> factors){
+    public boolean conatinsExactly2(ArrayList<Long> factors) {
         Set<Long> set = new HashSet<Long>(factors);
-        if(set.size()== factors.size()-1 && !factors.isEmpty()){
+        if (set.size() == factors.size() - 1 && !factors.isEmpty()) {
             return true;
-        }
-        else if(set.size() == factors.size()-2 && !factors.isEmpty()){
-            Collections.sort(factors);
-            for(int i=0; i< factors.size(); i++){
-                if (i+3 < factors.size() && factors.size() > 3 && factors.get(i) == factors.get(i + 3)) {
-                    return false;
-                }
+        } else if (set.size() == factors.size() - 2 && !factors.isEmpty()) {
+            if (factors.size() <= 3) {
+                return false;
             }
+
             return true;
+
         }
         return false;
 
